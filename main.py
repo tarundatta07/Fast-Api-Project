dc36e36c32da8e735f3e35843dc1d3cd17da9337
Tarun Datta <tarundattacse@gmail.com>
2023-12-24 01:45:55 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,3 @@ from routes.route import router
 
 app = FastAPI()
 app.include_router(router)
-    
-# uri = "mongodb+srv://admin:admin123@cluster0.yrmc5gf.mongodb.net/?retryWrites=true&w=majority"
-# # Create a new client and connect to the server
-# client = MongoClient(uri)
-# # Send a ping to confirm a successful connection
-# try:
-#     client.admin.command('ping')
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-#     print(e)
